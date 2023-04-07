ebec6d9babb58b1d452682f65ed266c8e3c0ae6d
Vikas Mourya <shiv.pal@surajinformatics.com>
2023-04-07 12:32:54 +0530

    refactoring in api constant file

--- app/src/main/java/com/sipl/rfidtagscanner/utils/ApiConstants.java
+++ app/src/main/java/com/sipl/rfidtagscanner/utils/ApiConstants.java
@@ -1,19 +1,16 @@
 package com.sipl.rfidtagscanner.utils;
 
 public class ApiConstants {
-    //    public static final String BASE_URL = "http://192.168.1.109:3000/coromandel-rest-repo/";
-//   public static final String BASE_URL = "http://192.168.1.202:8080/coromandel-rest-repo/";
-//  sudhir
-//    public static final String BASE_URL = "http://10.66.66.5:8089/coromandel-rest-repo/";
     public static final String BASE_URL = "http://10.66.66.90:8080/coromandel-rest-repo/";
 
-    //   login
-//    public static final String LOGIN = BASE_URL + "api/v1/auth/login/";
-    public static final String LOGIN = BASE_URL + "users/validate/";
+    // login
+    public static final String LOGIN = BASE_URL + "api/v1/auth/login/";
 
-
-    //   Loading Advise
+    // Loading Advise
     public static final String GET_LEP_NUMBER = BASE_URL + "RfidLepIssue/";
+
+    // Bothra loading advise
+    public static final String UPDATE_BOTHRA_LOADING_ADVISE = BASE_URL + "transactions/updateLoadingTransactions/";
     public static final String GET_SOURCE_LOCATION_DETAILS = BASE_URL + "source/getAllSourceDetails/";
     public static final String GET_DESTINATION_LOCATION_DETAILS = BASE_URL + "storageLocation/getAllStorageLocation/";
     public static final String GET_ALL_PINNACLE_SUPERVISOR = BASE_URL + "pinnacle/getAllPinnacleDetails/";
@@ -31,9 +28,5 @@ public class ApiConstants {
     public static final String GET_ALL_WAREHOUSE_NUMBER = BASE_URL + "storageLocation/getAllStorageLocationByPlantCode/";
     public static final String GET_ALL_REMARK = BASE_URL + "CoWhSupervisor/getAllRemarks/";
     public static final String UPDATE_WAREHOUSE_NUMBER = BASE_URL + "CoWhSupervisor/";
-
-//    Bothra loading advise
-    public static final String UPDATE_BOTHRA_LOADING_ADVISE = BASE_URL + "transactions/updateLoadingTransactions/";
-
 
 }
