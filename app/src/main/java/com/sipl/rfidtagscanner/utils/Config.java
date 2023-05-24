80d5778b77035064c1d58d3f516515ec2bb67a04
Vikas Mourya <vikas.m@surajinformatics.in>
2023-05-24 18:02:23 +0530

    Prod 1.0.1 apk (24-05-2023), Prod 1.0.0 apk (24-05-2023) - not release (roles issue)

--- app/src/main/java/com/sipl/rfidtagscanner/utils/Config.java
+++ app/src/main/java/com/sipl/rfidtagscanner/utils/Config.java
@@ -8,18 +8,18 @@ public class Config {
     public static final boolean isPlantDetailsRequiredInSideNav = true;
 
     //    Roles Constants
-    public static final String ROLES_BWH = "Bothra CIL Security Guard";
+/*    public static final String ROLES_BWH = "Bothra CIL Security Guard";
     public static final String ROLES_CWH = "Factory Warehouse Operator";
     public static final String ROLES_LAO = "LoadingAdvise Operator";
     public static final String ROLES_ADMIN_SUPER = "Super Admin";
-    public static final String ROLES_ADMIN_PLANT = "Plant Admin";
+    public static final String ROLES_ADMIN_PLANT = "Plant Admin";*/
 
 
-/*    public static final String ROLES_BWH = "Bothra CIL Security Guard";
+    public static final String ROLES_BWH = "Bothra CIL Security Guard";
     public static final String ROLES_CWH = "Factory Warehouse Operator";
     public static final String ROLES_LAO = "Coromandel LoadingAdvise Operator";
     public static final String ROLES_ADMIN_SUPER = "Super Admin";
-    public static final String ROLES_ADMIN_PLANT = "Plant Admin";*/
+    public static final String ROLES_ADMIN_PLANT = "Plant Admin";
 
 
     public static final String WRONG_CREDENTIALS = "User name or password Mismatch";
